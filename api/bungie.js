f6228b257c1701ff52ef00f80c818af814c86b71
DutchProgrammer <danny@dutchprogrammer.nl>
2014-11-13 23:39:42 +0100

    Added mongodb and asert

--- api/bungie.js
+++ api/bungie.js
@@ -1,45 +1,49 @@
 //185.10.50.140:9006
 
-var express    = require('express');
-var app        = express();
-var server     = require('http').createServer(app);
-var serverPort = 9006;
-var io         = require('socket.io')(server);
-var $          = require('jquery');
+var express     = require('express');
+var app         = express();
+var server      = require('http').createServer(app);
+var serverPort  = 9006;
+var io          = require('socket.io')(server);
+var MongoClient = require('mongodb').MongoClient;
+var assert      = require('assert');
 
 
 
-server.listen(serverPort);
+// Connection URL
+var url = 'mongodb://localhost:27017/';
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+
+	server.listen(serverPort);
 
 
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
+	io.on('connection', function (socket) {
 
-app.get('*', function (req, res) {
-	var url = (req.url === '/' ? '/' : req.url);
-	url = url.split('?')[0];
+	app.get('*', function (req, res) {
+		var url = (req.url === '/' ? '/' : req.url);
+		url = url.split('?')[0];
 
-	console.info(url, 'url');
+		console.info(url, 'url');
 
-	//Dont let them read this file !!
-	if (url === '/bungie.js') {
-		res.writeHead(404, {'Content-Type': 'text/html'});
-		res.write('404 file not found');
+		//Dont let them read this file !!
+		if (url === '/bungie.js') {
+			res.writeHead(404, {'Content-Type': 'text/html'});
+			res.write('404 file not found');
+			res.end();
+			return;
+		}
+		
+		
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write('<html><body>test</body><html>');
 		res.end();
-		return;
-	}
-	
-	console.info($('<html><body>test</body><html>').length, 'test');
-	
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.write('<html><body>test</body><html>');
-	res.end();
+	});
 
+
+	console.log('server is running on '+serverPort);
+
+  db.close();
 });
-
-
-console.log('server is running on '+serverPort);
